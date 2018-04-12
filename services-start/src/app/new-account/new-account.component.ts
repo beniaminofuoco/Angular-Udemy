08bc288a6757951d2261301b0f8e38ab7a334669
beniaminofuoco <d7393@italiaonline.it>
2018-04-12 14:35:43 +0200

    Services

---
+++ services-start/src/app/new-account/new-account.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+})
+export class NewAccountComponent {
+  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
+    this.accountsService.statusUpdate.subscribe(
+      (status: string) => alert('New Status: '+ status)
+    );
+   }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountsService.addAccount(accountName, accountStatus);
+  }
+}
