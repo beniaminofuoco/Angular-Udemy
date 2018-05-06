491c23d1311953f90493b90ed7fa027815fba82d
beniamino <beniamino.fuoco@gmail.com>
2018-05-06 20:54:40 +0200

    Form Reactive

---
+++ form-reactive/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit(){
+    // Inizializziamo il form programmaticamente andando ad associare
+    // ai vari componenti presenti in pagina dei controlli e relativi valori di default
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male')
+    });
+  }
+}
