5dc3055609680bc3c2a5799a1720174ed7ebe7b5
beniamino <beniamino.fuoco@gmail.com>
2018-05-14 07:27:56 +0200

    Reactive Form

--- form-reactive/src/app/app.component.ts
+++ form-reactive/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { VALID } from '@angular/forms/src/model';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,33 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
     // Inizializziamo il form programmaticamente andando ad associare
-    // ai vari componenti presenti in pagina dei controlli e relativi valori di default
+    // ai vari componenti presenti in pagina dei controlli e relativi valori di default.
+    // In questo approccio quando vogliamo associare delle properties
+    // (required, validatori) ad un componente front-end, specifichiamo TUTTO.
     this.signupForm = new FormGroup({
-      'username': new FormControl(null),
-      'email': new FormControl(null),
-      'gender': new FormControl('male')
+      // Il primo parametro del FormControl è l'eventuale valore di default 
+      // Il secondo è un validatore(o un array di validatori) legati al componente
+
+      // E'anche possibile annidare FormGroup, questo naturalmente deve avvenire anche
+      // a frot-end con l'utilizzo del tag FormGroupName
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     });
+  }
+
+  // Tale metodo viene associato al form in pagina tramite la 
+  // properties (ngSubmit)="onSubmit()
+  onSubmit(){
+    // Stampiamo il valore della variabile in console
+    console.log(this.signupForm);
+  }
+
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 }
