071f4727a9401c0517f3cf34f084e8539faff0ee
beniamino <beniamino.fuoco@gmail.com>
2018-05-20 12:36:12 +0200

    Crezione Validatore Custom

--- form-reactive/src/app/app.component.ts
+++ form-reactive/src/app/app.component.ts
@@ -10,6 +10,9 @@ import { VALID } from '@angular/forms/src/model';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  // Scriverò un validatore che controlla che l'Username NON SIA uno dei valori 
+  // presenti nell'Array
+  forbiddenUsers =['Beniamino', 'Rossella'];
 
   ngOnInit(){
     // Inizializziamo il form programmaticamente andando ad associare
@@ -23,7 +26,10 @@ export class AppComponent implements OnInit{
       // E'anche possibile annidare FormGroup, questo naturalmente deve avvenire anche
       // a frot-end con l'utilizzo del tag FormGroupName
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+         // Aggiungo il validatore al campo username 
+         // IMP -> Aggiungo il metodo bind al validatore per dire a Javascript 
+         // di utilizzare i validatori di questa classe
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]), 
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -41,5 +47,19 @@ export class AppComponent implements OnInit{
   onAddHobby(){
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  // Un validatore non è altro che una funzione che ritorna qualcosa
+  // Per validare un campo dobbiamo passare al validatore il controllo (control: FormControl)
+  // Il validatore restituirà una mappa con chiave di tipo String e valore boolean {[s: string]:boolean}
+  // IMPORTANTE -> se la validazione va a buon fine DOBBIAMO RESTITUIRE NULL oppure OMETTERE IL RETURN
+  forbiddenNames(control: FormControl):{[s: string]:boolean}{
+    // Se NON trova il valore, il metodo indexOf restituirà -1
+    if(this.forbiddenUsers.indexOf(control.value) !== -1){
+      // Il return contiene una chiave fittizia e il valore boolean 
+      // che indica se la validazione ha generato errore o meno.
+      return {nameIsForbidden : true};
+    }
+    return null;
   }
 }
